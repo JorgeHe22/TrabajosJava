e12068ba1d05051cc42fa19e1be7f239c757dec2
JorgeHe22 <jorgex.herrera24@gmail.com>
2025-02-27 23:43:22 -0500

    cajero

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,73 +4,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String[] productos = {"Cheetos", "Coca-cola", "Galletas", "Chocolatina", "Agua"};
-        int[] precios = {2000, 3000, 2500, 3500, 1500 };
-
-        Scanner scanner = new Scanner(System.in);
-        int totalCompra = 0;
-        int dineroIngresado = 0;
-        boolean sesionActiva = true;
+        double saldo = 50000;
+        Scanner teclado = new Scanner(System.in);
         int opcion = 0;
+        boolean continuar =  true;
 
-        while (sesionActiva){
-            System.out.println("MAQUINA EXPENDEDORA");
-            System.out.println("1. Ver productos disponibles");
+        System.out.println("Bienvenido al cajero automatico");
+
+        while(continuar){
+            System.out.println("Menú principal");
+            System.out.println("1. Consultar saldo");
             System.out.println("2. Ingresar dinero");
-            System.out.println("3. Seleccionar productos");
-            System.out.println("4. Terminar compra");
-            System.out.println("5.Salir. ");
-            System.out.println("Escoja un producto: ");
+            System.out.println("3. Retirar dinero");
+            System.out.print("4. Salir");
+            System.out.println("Seleccione una opcion: ");
 
-            opcion = scanner.nextInt();
-            scanner.nextLine();
+            opcion = teclado.nextInt();
 
             switch (opcion){
                 case 1:
-                    opcion = scanner.nextInt();
-                    System.out.println("PRODUCTOS DISPONIBLES");
-                    for (int i = 0; i < productos.length; i++){
-                        System.out.println((i+1) + ". " + productos[i] + ": $" + precios[i]);
-                    }
+                    System.out.println("Consulta de tu saldo");
+                    System.out.println("Su saldo actual es $");
                     break;
-
                 case 2:
-                    System.out.println("Ingrese la cantidad de dinero: $ ");
-                    int cantidad = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (cantidad > 0){
-                        dineroIngresado += cantidad;
-                        System.out.println("Dinero total ingresado: $ " + dineroIngresado);
+                    System.out.println("Ingresa tu dinero");
+                    System.out.println("Ingrese la cantidad a depositar $");
+                    double ingreso = teclado.nextDouble();
+                    if (ingreso > 0){
+                        saldo = saldo + ingreso;
+                        System.out.println("El deposito ha sido exitoso. Su nuevo saldo es: $" + saldo);
                     } else {
-                        System.out.println("Ingrese una cantidad valida");
+                        System.out.println("El valor no es valido");
                     }
-                    break;
-                case 3:
-                    if (dineroIngresado <= 0){
-                        System.out.println("Primero debe ingresar dinero. ");
-                        break;
-                    }
-                    System.out.println("Seleccione minimo 5 productos");
-                    int productosseleccionados = 0;
-
-                    while (productosseleccionados >= 5){
-                        System.out.println("Productos disponibles: ");
-                        for(int i = 0; i < productos.length; i++){
-                            System.out.println((i+1) + ". " + productos[i] + "$ " + precios[i]);
-                        }
-
-                    }
-
-
             }
-
-
         }
-
-
-
 
     }
 }
